2a1212ff59fc7ac95c7fbc91a6d6ebd4d2e277e8
hezuojiao <hezuojiao@gmail.com>
2019-10-29 22:28:00 +0800

    thread:

--- main.cpp
+++ main.cpp
@@ -5,22 +5,16 @@
 
 int main(int argc, char* argv[]) {
 
-  auto customer = new Customer();
-  auto order = new Order();
-  auto lineitem = new Lineitem();
+  Customer customer;
+  Order order;
+  Lineitem lineitem;
 
   bool rebuild = !util::file_exists(O_ORDERKEY_PATH.c_str());
   typedef std::chrono::duration<float> float_seconds;
   auto start = std::chrono::system_clock::now();
-  std::thread thread1 = std::thread([customer, rebuild, argv] {
-    customer->buildCache(argv[1], rebuild);
-  });
-  std::thread thread2 = std::thread([order, rebuild, argv] {
-    order->buildCache(argv[2], rebuild);
-  });
-  std::thread thread3 = std::thread([lineitem, rebuild, argv] {
-    lineitem->buildCache(argv[3], rebuild);
-  });
+  std::thread thread1 = std::thread([&] { customer.buildCache(argv[1], rebuild); });
+  std::thread thread2 = std::thread([&] { order.buildCache(argv[2], rebuild); });
+  std::thread thread3 = std::thread([&] { lineitem.buildCache(argv[3], rebuild); });
   thread1.join();
   thread2.join();
   thread3.join();
@@ -37,14 +31,14 @@ int main(int argc, char* argv[]) {
 
   start = std::chrono::system_clock::now();
   for (int i = 0; i < thread_num; i++) {
-    threads[i] = std::thread([customer, order, lineitem, i, round, thread_num, &results, &argv]{
+    threads[i] = std::thread([&, i] {
       for (int round_id = i; round_id < round; round_id += thread_num) {
         auto mktsegmentCondition = argv[5 + round_id * 4][0];
         auto orderdateCondition = util::DateToInt(argv[5 + round_id * 4 + 1]);
         auto shipdateCondition = util::DateToInt(argv[5 + round_id * 4 + 2]);
         auto topn = std::stoi(argv[5 + round_id * 4 + 3]);
-        auto executor = new Executor(customer, order, lineitem, mktsegmentCondition, orderdateCondition, shipdateCondition, topn);
-        results[round_id] = executor->getResult();
+        Executor executor(customer, order, lineitem, mktsegmentCondition, orderdateCondition, shipdateCondition, topn);
+        results[round_id] = executor.getResult();
       }
     });
   }
